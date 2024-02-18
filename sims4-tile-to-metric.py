011e4563c8fc9a30c5c248c1c241f16a497ca274
Caio Patriota <caiopatriotabarros@gmail.com>
2024-02-17 22:54:01 -0300

    added a while loop

--- sims4-tile-to-metric.py
+++ sims4-tile-to-metric.py
@@ -1,11 +1,21 @@
-#Criado para converter "tiles" do jogo The Sims 4 para metros.
-#Nos jogos The Sims, um "tile" representa 3 pés (cerca de 0,9144 metros). Um "tile" do The Sims 4 é equivalente a aproximadamente 0,8361 metros quadrados. Se você deseja converter para metros, cada "tile" seria aproximadamente 0,2787 metros de cada lado.
+# Criado para converter "tiles" do jogo The Sims 4 para metros.
+# Nos jogos The Sims, um "tile" representa 3 pés (cerca de 0,9144 metros). Um "tile" do The Sims 4 é equivalente a aproximadamente 0,8361 metros quadrados. Se você deseja converter para metros, cada "tile" seria aproximadamente 0,2787 metros de cada lado.
 
-meters = 0.2787;
-print("Bem vindo ao conversor de tiles para metros!");
+# Função para verificar se a entrada é um número
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
-metersQuantity = input("Digite quantos metros deseja converter: ");
+meters = 0.2787
+print("Bem vindo ao conversor de tiles para metros!")
 
-tiles = float(metersQuantity) / meters;
+metersQuantity = input("Digite quantos metros deseja converter: ")
 
-print("Para", metersQuantity, "metros", "a quantidade de tiles é:", int(tiles));
+while not is_number(metersQuantity):
+    metersQuantity = input("Digite quantos metros deseja converter: ")
+
+tiles = float(metersQuantity) / meters
+print("Para", metersQuantity, "metros", "a quantidade de tiles é:", int(tiles))
