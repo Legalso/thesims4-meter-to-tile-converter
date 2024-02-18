68a70b1fdc6675b97250d88742ed101cd32ab1ff
Caio Patriota <caiopatriotabarros@gmail.com>
2024-02-17 23:33:58 -0300

    rounding

--- sims4-tile-to-metric.py
+++ sims4-tile-to-metric.py
@@ -1,5 +1,5 @@
-# Criado para converter "tiles" do jogo The Sims 4 para metros.
-# Nos jogos The Sims, um "tile" representa 3 pés (cerca de 0,9144 metros). Um "tile" do The Sims 4 é equivalente a aproximadamente 0,8361 metros quadrados. Se você deseja converter para metros, cada "tile" seria aproximadamente 0,2787 metros de cada lado.
+# Convertor de "tiles" do The Sims 4 para metros (m).
+# Nos jogos The Sims, um "tile" representa 3 pés (cerca de 0,9144 metros). Um "tile" do The Sims 4 é equivalente a aproximadamente 0,8361 metros quadrados. Se você deseja converter para metros, cada "tile" seria aproximadamente 0,2787 metros de cada lado. Usamos a metade de um "tile" para calcular a área.
 
 # Função para verificar se a entrada é um número
 def is_number(s):
@@ -17,6 +17,9 @@ metersQuantity = input("Digite quantos metros deseja converter: ")
 while not is_number(metersQuantity):
     metersQuantity = input("Digite quantos metros deseja converter: ")
 
-#every tile tem dois quadradinhos. no começo pensei nesse quadradinho como um tile, mas na verdade ele é metade de um tile. por isso a divisão por 2.
-tiles = (float(metersQuantity) / meters) /2
-print("Para", metersQuantity, "metros", "a quantidade de tiles é:", int(tiles))
+#converte a string do input em float e calcula a quantidade quantos metros tem em tiles.
+#um tile tem dois quadradinhos. no começo pensei nesse quadradinho como um tile, mas na verdade ele é metade de um real tile. por isso a divisão por 2.
+tiles = (float(metersQuantity) / meters) / 2
+#arredondar para cima se passar de 0.5, dessa maneira exibe apenas um número inteiro (ajuda a visualizar melhor).
+tiles = round(tiles)
+print("Para", metersQuantity, "metros", "a quantidade de tiles é:", tiles)
